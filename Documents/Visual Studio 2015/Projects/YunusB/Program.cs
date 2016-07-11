821647cad59e6b931e2b180f69527ee248adbb95
Yunus Mohammad <myunus@nexstra.com>
2016-07-11 02:55:31 +0000

    comiite

--- Documents/Visual Studio 2015/Projects/YunusB/Program.cs
+++ Documents/Visual Studio 2015/Projects/YunusB/Program.cs
@@ -18,6 +18,8 @@ namespace YunusB
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             //tst;
+
+            //ths i stest
         }
     }
 }
